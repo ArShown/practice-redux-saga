f104cb66bc924698f84bb38da145a9d16ed96faa
ArShown <arshown@megalive.com.tw>
2018-06-04 09:06:38 +0800

    優化元件與更新串接協定

--- app/dist/components/member/handler.js
+++ app/dist/components/member/handler.js
@@ -1,18 +1,22 @@
 /* @flow */
 import { compose, withStyle } from '~/core/container';
-import { withPropsOnChange, withState } from 'recompose';
+import { branch, renderNothing, withPropsOnChange, withState } from 'recompose';
 import member from './member.scss';
 import { join } from 'ramda';
 
-export default compose(
-  withPropsOnChange(['name'], () => {
-    const _getColorCode = () => {
-      const color = () => parseInt(Math.random() * 255 + 1).toString(16);
-      return join('', [color(), color(), color()]);
-    };
+export default branch(
+  ({ name }) => !!name,
+  renderNothing,
+  compose(
+    withPropsOnChange(['name'], () => {
+      const _getColorCode = () => {
+        const color = () => parseInt(Math.random() * 255 + 1).toString(16);
+        return join('', [color(), color(), color()]);
+      };
 
-    return { src: 'http://placehold.it/150/' + _getColorCode() };
-  }),
-  withState('loaded', 'setLoaded', false),
-  withStyle(member)
+      return { src: 'https://placehold.it/150/' + _getColorCode() };
+    }),
+    withState('loaded', 'setLoaded', false),
+    withStyle(member)
+  )
 );
